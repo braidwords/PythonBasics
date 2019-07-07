46a8b5c34898ee9fd08056dd580e8228bf0d969d
kusumnegi <kusumnegi2006@gmail.com>
2019-07-07 20:26:38 +0200

    First Python code.

--- Weightconverter_project.py
+++ Weightconverter_project.py
@@ -2,9 +2,9 @@ weight = input("Enter your weight: ")
 weight_unit = input("Choose the unit:(P)ounds or (K)ilograms: ")
 if weight_unit.upper() == 'K':
     weight = float(weight) * 2.205
-    print(weight)
+    print(f"You are {weight} pounds")
 else:
     weight = float(weight) / 2.205
-    print(weight)
+    print(f"You are {weight} kilos")
 
 print("Conversion finished")
