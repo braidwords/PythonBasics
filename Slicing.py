4ed2eb875b337728cfdf8e085b610e8b42a1b730
Kusum Negi <kusumnegi2006@gmail.com>
2019-07-15 20:27:41 +0200

    Basic concept of slicing and stride in python.

---
+++ Slicing.py
@@ -0,0 +1,17 @@
+user_data = "Good Vibes"
+print(user_data[3])
+print(user_data[2:5])
+print(user_data[:])
+print(user_data[:9])
+print(user_data[0:])
+print(user_data[-4])
+print(user_data[-4:-1])
+print(user_data[::-1])
+print(user_data[-7:])
+#Stride in Slice
+print(user_data[:8:3])
+print(user_data[::-1])
+print(user_data[::-3])
+
+
+
