dddbe1858503fe94a3a2ddfbea8b3d358f12b3a4
Kusum Negi <kusumnegi2006@gmail.com>
2019-07-17 15:41:56 +0200

    Module examples. COnverters is imported in the program written in Module Example file.

---
+++ converters.py
@@ -0,0 +1,8 @@
+def kg_to_pounds(weight):
+    weight = weight * 2.205
+    print(f"You are {weight} pounds")
+
+def pounds_to_kg(weight):
+    weight = weight / 2.205
+    print(f"You are {weight} kilos")
+
